dc1d1d41e6d4bd32a517915695bbd8f6a142e2f1
Attica-oss <g.mounac@outlook.com>
2023-06-18 09:47:02 +0400

    clean the module

--- sql_database/convert_to_sql.py
+++ sql_database/convert_to_sql.py
@@ -1,3 +1,5 @@
+'covert_to_sql.py'
+
 import sys
 import pandas as pd
 import sqlalchemy
@@ -6,10 +8,12 @@ from sql_database.config import CONNECTION_STRING
 sys.path.append('sql_database/config')
 
 class DataFrameLoader:
+    ''' Load data'''
     def __init__(self, connection_string):
         self.connection_string = connection_string
-    
+
     def load_dataframe(self, dataframe, table_name):
+        'creare engine'
         engine = sqlalchemy.create_engine(self.connection_string)
         dataframe.to_sql(table_name, engine, if_exists='replace', index=False)
 
@@ -17,7 +21,7 @@ class DataFrameLoader:
 if __name__ == '__main__':
     # Create an instance of DataFrameLoader with the connection string from the config file
     loader = DataFrameLoader(CONNECTION_STRING)
-  
+
     # Load a DataFrame into an SQL table
     df = pd.DataFrame({'column1': [1, 2, 3], 'column2': ['a', 'b', 'c']})
     loader.load_dataframe(df, 'your_table_name')
